461062df6ed360603d8b7cefee6f4f306e253ab9
Ryan Nograles <ryannograles.025.official@gmail.com>
2025-10-30 16:58:16 +0800

    modify

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ import "leaflet/dist/leaflet.css";
 
 // Fix for Leaflet default icon paths for many bundlers (optional but recommended)
 import L from "leaflet";
-import AppRouter from "@/app/routes/AppRoutes";
+import AppRouter from "./app/routes/AppRoutes";
 
 const iconUrl = "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png";
 const iconUrl2 = "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png";
